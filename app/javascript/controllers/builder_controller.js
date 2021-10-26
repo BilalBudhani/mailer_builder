70058187480c32fe681293e145d6cbb869d0a9e2
Bilal Budhani <bilalbudhani@gmail.com>
2021-10-26 13:38:37 +0400

    Wires up title

--- app/javascript/controllers/builder_controller.js
+++ app/javascript/controllers/builder_controller.js
@@ -1,9 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['header', 'subHeader', 'banner']
+  static targets = ['title']
 
   connect() {
     console.log('builder is connected')
+    this.refresh = this.refresh.bind(this)
+  }
+
+  refresh() {
+    console.log(this.titleTarget.value)
+
+    const mailerTitle = document.querySelector('#mailer_title')
+    mailerTitle.innerHTML = this.titleTarget.value
   }
 }
